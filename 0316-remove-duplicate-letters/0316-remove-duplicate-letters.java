b3b810c484e01075fc5b15f585c0bbd56ba4b457
Bathineni Vishwa Sai <vishwa.bathineni@gmail.com>
2026-07-19 09:12:07 +0530

    Time: 3 ms (31.76%), Space: 44.1 MB (22.16%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public String removeDuplicateLetters(String s) {
+
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        HashSet<Character> set = new HashSet<>();
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']--;
+
+            if (set.contains(ch)) {
+                continue;
+            }
+
+            while (!stack.isEmpty()
+                    && ch < stack.peek()
+                    && freq[stack.peek() - 'a'] > 0) {
+
+                set.remove(stack.pop());
+            }
+
+            stack.push(ch);
+            set.add(ch);
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
